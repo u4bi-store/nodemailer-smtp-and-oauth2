28e45cfef8731157f18c881a94daff1e2ade69df
u4bi <rootcode10@gmail.com>
2017-12-05 23:30:09 +0000

    smtp app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const porter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
+});
+
+const mail = {
+    from    : `Handsome User <${ process.env.MAIL_USER }>`,
+    to      : 'rootcode10@gmail.com',
+    subject : 'subject test',
+    // text    : 'text test',
+    html    : `
+    <h1 style="color:#ff00ff;">hi u4bi</h1>
+    <img src="https://avatars0.githubusercontent.com/u/21367010?s=460&v=4">
+    `
+};
+
+porter.sendMail(mail, (error, info) => {
+    if(error)return console.log('error', error);
+
+    console.log('sent', info);
+});
